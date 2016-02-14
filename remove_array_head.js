50489a2420ae8da0de116449b1d8d73d664df71b
Brandon Yen <Aiumi@users.noreply.github.com>
2016-02-13 18:50:26 -0800

    Create remove_array_head.js
    Function that removes a specific number of elements from the head of an array

---
+++ remove_array_head.js
@@ -0,0 +1,5 @@
+function slasher(arr, howMany){ // the function takes 2 parameters: the array and the number of elements to be removed from its head
+  	return arr.slice(howMany); // returns the array starting from index[howMany], so the array from index 0 to howMany is removed
+  } 
+
+slasher(); // enter the array here
